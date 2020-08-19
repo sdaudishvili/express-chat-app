f340a521643a75154632f3a09cd36037edb5ecc7
sdaudishvili <sdaudishvili@gmail.com>
2020-08-19 18:20:03 +0400

    fix port

--- app.js
+++ app.js
@@ -47,6 +47,8 @@ io.on('connection', (socket) => {
   });
 });
 
-http.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+http.listen(port, () => {
   console.log('listening on *:3000');
 });
